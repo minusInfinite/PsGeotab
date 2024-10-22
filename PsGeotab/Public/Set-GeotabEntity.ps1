119feb110fa9be0b652c94742a63733471fe20fa
minusInfinite <ashley@minusinfinite.net>
2024-10-22 23:00:47 +1030

    update function names,exports and examples

--- PsGeotab/Public/Set-GeotabEntity.ps1
+++ PsGeotab/Public/Set-GeotabEntity.ps1
@@ -1,12 +1,37 @@
+<#
+.SYNOPSIS
+Modify a Geotab Enitiy
+
+.PARAMETER Session
+Geotab session object.
+
+.PARAMETER typeName
+The Entity type
+
+.PARAMETER Entity
+The Enitiy Data 
+
+.EXAMPLE
+PS> Set-GeotabEntity -Session $Session -typeName 'User' -entity $UserData
+
+Submit an update to the provided User definded in $UserData
+
+.NOTES
+To Archive a User or Device set the ToDate of that user a current Datetime.
+
+.LINK
+https://developers.geotab.com/myGeotab/apiReference/methods/Set
+
+#>
 function Set-GeotabEntity {
 
     [CmdletBinding(SupportsShouldProcess)]
     param (
-		[Parameter(Mandatory)]
-		[PsCustomObject]$Session,
+        [Parameter(Mandatory)]
+        [PsCustomObject]$Session,
 
         [Parameter(Mandatory)]
-		[string]$typeName,
+        [string]$typeName,
 
         [Parameter(Mandatory)]
         [object]$entity
@@ -32,8 +57,7 @@ function Set-GeotabEntity {
         # payload as JSON
         Write-Debug ($Body | ConvertTo-Json -Depth 4)
     
-        if ( $PSCmdlet.ShouldProcess("Invoke-WebRequest") )
-        {
+        if ( $PSCmdlet.ShouldProcess("Invoke-WebRequest") ) {
             # POST
             $Content = ( Invoke-WebRequest -Uri $uri -Method Post -Body ($Body | ConvertTo-Json -Depth 4) -ContentType "application/json" ).Content | ConvertFrom-Json
         
